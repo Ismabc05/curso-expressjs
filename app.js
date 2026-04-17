4d8eddd1d72586762b2423766a7607b36f14740f
Ismael <ismael@example.com>
2026-04-17 11:36:16 +0200

    configuracion expressjs

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send(`
+        <h1>Curso Express.js V2</h1>
+        <p> Esto es una aplicaccion nodejs con expressjs</p>
+        <p> Corre en el puerto ${PORT}</p>
+        `);
+});
+
+
+app.listen(PORT, () => {
+    console.log("Servidor corriendo")
+});
