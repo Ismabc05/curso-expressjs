d77df9a6d6bcdf1ceb4e179011bf23ab398e121e
Ismael <ismael@example.com>
2026-04-18 11:47:34 +0200

    implementacion de dos rutas y body-parser

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 require("dotenv").config() // con esto puedo leer las variables de entorno
 const express = require("express")
+const bodyParser = require("body-parser")
 
 const app = express();
+app.use(bodyParser.json()) // Permite leer JSON del body
+app.use(bodyParser.urlencoded({ extended: true })) // Permite leer datos de formularios
+
+// “Gracias a body-parser, los datos del body (JSON o formularios) se parsean y se convierten en objetos JavaScript accesibles en req.body.”
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,6 +17,21 @@ app.get("/", (req, res) => {
         <p> Corre en el puerto ${PORT}</p>
         `);
 });
+
+app.get("/users/:id", (req, res) => {
+    const userID = req.params.id; // tenemos acceso al id de la ruta gracias a params
+    res.send(`Mostar informacion del usuario con el id: ${userID}`)
+});
+
+app.get("/search", (req, res) => {
+    const terms = req.query.termino || "No especificado" // obtiene el valor despues  del ? en termino
+    const category = req.query.categoria || "Todas" // obtiene el valor despues del ? en cateogria
+    res.send(`
+        <h2>Resultados de busqueda</h2>
+        <p>Termino: ${terms}</p>
+        <p>Categoria: ${category}</p>
+        `)
+})
 
 
 app.listen(PORT, () => {
