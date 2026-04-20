bb5efa37bbf107d1b72c161d2cca68f7ddc07872
Ismael <ismael@example.com>
2026-04-20 11:35:02 +0200

    completada la configuracion

--- app.js
+++ app.js
@@ -33,6 +33,41 @@ app.get("/search", (req, res) => {
         `)
 })
 
+app.post("/login", (req, res) => {
+    const usuario = req.body.usuario
+    const contraseña = req.body.contraseña
+
+    res.json({
+        message: "Login recibido",
+        data: usuario
+    })
+})
+
+app.post("/form", (req, res) => {
+    const name = req.body.nombre || "Anonimo"
+    const email = req.body.email || "No especificado"
+    res.json({
+        message: "Datos Recibidos",
+        data: {
+            name,
+            email
+        }
+    })
+})
+
+app.post("/api/data", (req, res) => {
+    const data = req.body
+
+    if(!data || Object.keys(data).length === 0) {
+        return res.status(400).json({error: "No se recibieron datos"});
+    }
+
+    res.status(201).json({
+        message: "Datos JSON recibidos",
+        data: data
+    })
+})
+
 
 app.listen(PORT, () => {
     console.log("Servidor corriendo")
